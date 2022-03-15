059e6a8d0f1ec9158d1946486f11b44140b5c4e3
Rushikesh Patil <rushipatil6632@gmail.com>
2022-03-15 11:51:02 +0530

    Added:Ability to analyse mood and respond either happy or sad

--- src/main/java/com/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/moodanalyser/MoodAnalyser.java
@@ -2,18 +2,10 @@ package com.moodanalyser;
 
 public class MoodAnalyser {
 
-	private String msg;
-    public MoodAnalyser() {
-    }
-
-    public MoodAnalyser(String msg){
-        this.msg = msg;
-    }
-
-    public String analyzeMood() {
-        if(msg.contains("Sad")){
-            return "Sad";
-        }
-        return "Happy";
-    }
+	 public String analyseMood(String msg) {
+	        if(msg.contains("Sad")){
+	            return "Sad";
+	        }
+	        return "Happy";
+	    }
 }
--- src/test/java/com/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -4,22 +4,18 @@ import org.junit.Test;
 import org.junit.Assert;
 
 
- //Ability to analyse and respond Happy or Sad Mood
- 
-
 public class MoodAnalyserTest {
     @Test
     public void givenMessage_WhenSad_ShouldReturnSad(){
-        MoodAnalyser moodAnalyzer = new MoodAnalyser("I am in Sad Mood");
-        String mood = moodAnalyzer.analyzeMood();
+    	MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in Sad Mood");
         Assert.assertEquals("Sad",mood);
     }
 
     @Test
     public void givenMessage_WhenHappy_ShouldReturnHappy(){
-        MoodAnalyser moodAnalyzer = new MoodAnalyser("I am in Happy Mood");
-        String mood = moodAnalyzer.analyzeMood();
+    	MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in Happy Mood");
         Assert.assertEquals("Happy",mood);
     }
-
 }
